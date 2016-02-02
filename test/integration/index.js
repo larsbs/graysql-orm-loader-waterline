c39d540b44bb1914cd39ec7a6bdcfcfc97b73e00
Lorenzo Ruiz <lars.bs@hotmail.com>
2016-02-02 21:58:08 +0100

    All basic queries tests passing

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const expect = require('chai').expect;
 const path = require('path');
 const rmdir = require('rmdir');
 const graphql = require('graphql');
@@ -49,20 +50,105 @@ module.exports = function () {
 
     describe('Basic Queries', function () {
       it('should allow us to query for all the groups', function (done) {
-        console.log(GraphQLUtils.printSchema(Schema));
-        models.group.find().then(console.log);
         const query = `query GetGroups {
           groups {
             id,
             name
           }
         }`;
+        const expected = {
+          data: {
+            groups: [{
+              id: 1,
+              name: 'Group 1'
+            }, {
+              id: 2,
+              name: 'Group 2'
+            }]
+          }
+        };
         graphql.graphql(Schema, query)
         .then(result => {
-          console.log(result);
+          expect(result).to.deep.equal(expected);
+          done();
         })
         .catch(err => done(err));
       });
+      it('should allow us to query for all the users', function (done) {
+        const query = `query GetUsers {
+          users {
+            id,
+            nick
+          }
+        }`;
+        const expected = {
+          data: {
+            users: [{
+              id: 1,
+              nick: 'Lars'
+            }, {
+              id: 2,
+              nick: 'Deathvoid'
+            }, {
+              id: 3,
+              nick: 'Grishan'
+            }]
+          }
+        };
+        graphql.graphql(Schema, query)
+        .then(result => {
+          expect(result).to.deep.equal(expected);
+          done();
+        })
+        .catch(err => done(err));
+      });
+      it('should allow us to query for a single group', function (done) {
+        const query = `query GetGroup {
+          group(id: 1) {
+            id,
+            name
+          }
+        }`;
+        const expected = {
+          data: {
+            group: {
+              id: 1,
+              name: 'Group 1'
+            }
+          }
+        };
+        graphql.graphql(Schema, query)
+        .then(result => {
+          expect(result).to.deep.equal(expected);
+          done();
+        })
+        .catch(err => done(err));
+      });
+      it('should allow us to query for a single user', function (done) {
+        const query = `query GetUser {
+          user(id: 1) {
+            id,
+            nick
+          }
+        }`;
+        const expected = {
+          data: {
+            user: {
+              id: 1,
+              nick: 'Lars'
+            }
+          }
+        };
+        graphql.graphql(Schema, query)
+        .then(result => {
+          expect(result).to.deep.equal(expected);
+          done();
+        })
+        .catch(err => done(err));
+      });
+    });
+
+    describe('Relationship Queries', function () {
     });
 
   });
